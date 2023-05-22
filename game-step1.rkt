d5ac439e243f7a29ed93541abb3039981386ec88
Matthias Felleisen <matthias@ccs.neu.edu>
2023-05-22 16:31:13 -0400

    factor out handlers for solo play

--- game-step1.rkt
+++ game-step1.rkt
@@ -1,16 +1,13 @@
 #lang racket
 
-;; exploratory visualization 
+;; display and handle the game for a solo human player 
 
 ;; ---------------------------------------------------------------------------------------------------
 (require PillWars/Common/state)
+(require PillWars/World/handlers-for-human-nav)
 (require PillWars/World/constants)
 
 (require 2htdp/universe)
-(require 2htdp/image)
-
-(module+ test
-  (require (submod PillWars/Common/state examples)))
 
 ;; ---------------------------------------------------------------------------------------------------
 ;; {String -> State}
@@ -25,38 +22,6 @@
     [on-key    navigate-by-key]
     [name      my-name]
     [stop-when game-over? (draw-state BG)]))
-
-#; {State KeyEvent -> State}
-;; allow player to navigate the game space via keystrokes:
-;; -- ↑ for forward
-;; -- ← for left
-;; -- → for right
-;; -- SPACE for "fire" ("destroy enemy" that the fighter is sitting on)
-(define (navigate-by-key s ke)
-  (cond
-    [(key=? " " ke)     (eat-my-fighter s)]
-    [(key=? "up" ke)    (move-my-fighter s)]
-    [(key=? "right" ke) (rotate-my-fighter s (/ pi -60))]
-    [(key=? "left" ke)  (rotate-my-fighter s (/ pi +60))]
-    [else s]))
-
-#; {State N N MouseEvent -> State}
-;; allow player to navigate the game space via mouse clicks:
-;; -- button-down in the yellow space of a fighter changes its direction
-;; -- button-down in any white space moves "my" fighter straight ahead
-;; -- button-down on an "enemy" "fires" IF the fighter is "on" the enemy 
-(define (turn-by-mouse s x y me)
-  (cond
-    [(mouse=? "button-down" me)
-     (cond
-       [(state-mouse-click-ok? s x y) => (λ (θ) (rotate-my-fighter s (- θ)))]
-       [(mouse-click-on-pill? s x y) => (λ (pill) (eat-my-fighter s pill))]
-       [else (move-my-fighter s)])]
-    [else s]))
-
-#; {State -> Boolean}
-(define (game-over? s)
-  (or (empty? (state-fighters s)) (empty? (state-pills s))))
 
 ;; ---------------------------------------------------------------------------------------------------
 (module+ test
