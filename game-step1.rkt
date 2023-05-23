74f0d594fea39bda2e2c1d289236cb9e2f010c46
Matthias Felleisen <matthias@ccs.neu.edu>
2023-05-23 12:33:42 -0400

    This is an attempt to implement a turn-based game when the two players use distinct forms to interact with the world state, e.g.,
     -- one reacts on tick events
     -- the other reacts to mouse events and/or key strokes.
    
    My solution is brittle (introducing turns, using game-specific knowledge);
    perhaps there is a better one ..
    
    Once I had the solution, it turns out that the game might not be
    termination if the other player is forced to eat all enemies. There
    will be situations when no strategy works (perhaps the step-size
    is too large. I need to check.)

--- game-step1.rkt
+++ game-step1.rkt
@@ -4,6 +4,7 @@
 
 ;; ---------------------------------------------------------------------------------------------------
 (require PillWars/Common/state)
+(require PillWars/Common/fighter)
 (require PillWars/World/handlers-for-human-nav)
 (require PillWars/AI/strategy-1)
 (require PillWars/World/constants)
@@ -34,7 +35,9 @@
   (interactive-winners end-with))
 
 ;; ---------------------------------------------------------------------------------------------------
-;; making a `big-bang` world a two-player game, with one of them an AI 
+;; making a `big-bang` world a two-player game, with one of them an AI
+;; ASSUME the AI can't make the mistake of dropping out
+;; CONSEQUENCE if the Human player drops out, we notice because the AI player is first. 
 
 (struct interactive [whose-turn state] #:transparent)
 #; {type Interactive = [interactive Tag State]}
@@ -42,22 +45,30 @@
 ;; INVARIANT `whose-turn` and the first player in `state` must be in sync
 #; {type [Handler X] = (State Any ... -> X)}
 
-(define AI (gensym "AI-"))
+(define AI (gensym "AI"))
 (define HUMAN (gensym "HUMAN"))
 
 #; {State -> Interactive}
 ;; add an AI player and set it up to go first 
 (define (create-interactive state)
-  (interactive AI (add-player-to-front "AI" state)))
+  (interactive AI (add-player-to-front AI state)))
 
 #; {Tag Tag [Handler State] -> Interactive Any ... -> Interactive}
 (define ((enable tag other handler) i . others)
   (match-define [interactive whose state0] i)
   (cond
+    [(only-one-player state0)
+     (define state++ (apply handler state0 others))
+     (interactive AI state++)]
     [(equal? whose other) i]
     [else
      (define state++ (apply handler state0 others))
      (interactive other (swap-two-players state++))]))
+
+#; {State -> Boolean}
+(define (only-one-player state0)
+  (define players (state-fighters state0))
+  (and (cons? players) (empty? (rest players))))
 
 #; {[Hander X] -> Interactive Any ... -> X}
 (define [(strip handler) i . others]
@@ -67,7 +78,7 @@
 #; {Interactive -> [Listof String]}
 (define (interactive-winners i)
   (match-define [interactive whose state] i)
-  (pretty-print state)
+  (pretty-print i)
   (winners state))
   
 
