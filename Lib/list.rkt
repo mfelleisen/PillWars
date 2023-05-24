308ec1702a42865cf3896b79b33d0eda97b6cf28
Matthias Felleisen <matthias@ccs.neu.edu>
2023-05-24 19:32:54 -0400

    add remove-ref, should go into SwDev/Lib

--- Lib/list.rkt
+++ Lib/list.rkt
@@ -1,13 +1,25 @@
 #lang racket
 
-(provide
- #; {[NEListof X] -> [NEListof X]}
- list-rotate)
+(define (good-index? lox)
+  (flat-named-contract "proper index" (λ (i) (> (length lox) i))))
 
-;; ----------------------------------------------------------------------------------------
+(provide
+ (contract-out
+  [list-rotate (-> (and/c cons? list?) (and/c cons? list?))]
+  [remove-ref (->i ([lox list?] [i (lox) (and/c natural? (good-index? lox))]) (result list?))]))
+
+;; ---------------------------------------------------------------------------------------------------
 (define (list-rotate lox)
   (snoc (rest lox) (first lox)))
 
 (define (snoc l x) (append l (list x)))
   
-  
+(define (remove-ref lox0 i0)
+  (let rem ([i i0] [lox lox0])
+    (cond
+      [(zero? i) (rest lox)]
+      [else (cons (first lox) (rem (sub1 i) (rest lox)))])))
+
+(module+ test
+  (require (submod ".."))
+  (remove-ref '[a] 1))
