f79946621e3e61ba1b70cc2ee7b4220cb3f0d761
Matthias Felleisen <matthias@ccs.neu.edu>
2023-05-26 17:15:59 -0400

    enable AI players to give up

--- Common/action.rkt
+++ Common/action.rkt
@@ -11,16 +11,18 @@
  
  (struct-out mov)
  (struct-out eat)
- (struct-out rot))
+ (struct-out rot)
+ (struct-out gup))
 
 ;; -----------------------------------------------------------------------------
 ;; universe --> world messages: 
 (struct your-turn [state] #:prefab)
 
 ;; world --> universe messages: 
-(struct mov [posn] #:prefab)
-(struct eat [posn] #:prefab)
-(struct rot [angle] #:prefab)
+(struct mov [any] #:prefab)   ;; move according to current speed 
+(struct eat [posn] #:prefab)  ;; eat the pill at/near this location 
+(struct rot [angle] #:prefab) ;; rotate by this angle 
+(struct gup [why] #:prefab)   ;; giving up (why)
 
 (define (action? a)
-  (or (mov? a) (eat? a) (rot? a)))
+  (or (mov? a) (eat? a) (rot? a) (gup? a)))
